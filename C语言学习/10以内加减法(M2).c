8f0e862c4b1e3e7feafc2ea8d4f28fbff0a31572
zcj924 <zcj924@gmail.com>
2019-04-08 06:26:44 +0800

    可以指定出题数目

--- C语言学习/10以内加减法(M2).c
+++ C语言学习/10以内加减法(M2).c
@@ -5,19 +5,25 @@
 
 int main(int argc, char const* argv[])
 {
-	int a[100], b[100], x[100], j;
+	int i;
+	printf("请输入你要出多少道题目,文件保存在程序目录下\n");
+	scanf_s("%d", &i);
+
+	
+	int a[10000], b[10000], x[10000], j;
 	srand((unsigned)time(NULL));//用时间初始化随机数种子库,unsigned无符号型
 	FILE* fp1,*fp2;
+	//fp1 = fopen("1.txt", "w"); ????夹私货?fopen不让用????????
 	fopen_s(&fp1, "没有答案的题目.txt", "w");
 	fopen_s(&fp2, "有答案的题目.txt", "w");
 
-	for (j = 0; j < 100; j++)//生成随机数放进数组中
+	for (j = 0; j < i; j++)//生成随机数放进数组中
 	{
 		a[j] = rand() % 10;
 		b[j] = rand() % 10;
 		x[j] = rand() % 2;
 	}
-	for (j = 0; j < 100; j++)//没有答案的题目
+	for (j = 0; j < i; j++)//没有答案的题目
 	{
 	
 		switch (x[j]) {
@@ -34,7 +40,7 @@ int main(int argc, char const* argv[])
 		}
 	}
 	
-	for (j = 0; j < 100; j++)//有答案的题目
+	for (j = 0; j < i; j++)//有答案的题目
 	{
 		
 		switch (x[j]) {
@@ -52,5 +58,7 @@ int main(int argc, char const* argv[])
 	}
 	fclose(fp1);
 	fclose(fp2);
+	if (i > 100)
+		printf("你怕是让小学生累死哦!!");
 	return 0;
 }
