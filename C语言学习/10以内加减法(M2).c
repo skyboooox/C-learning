733b851f071770227b0cc61379b67f2775616ad1
zcj924 <zcj924@gmail.com>
2019-04-08 06:14:43 +0800

    添加文件操作

--- C语言学习/10以内加减法(M2).c
+++ C语言学习/10以内加减法(M2).c
@@ -7,42 +7,50 @@ int main(int argc, char const* argv[])
 {
 	int a[100], b[100], x[100], j;
 	srand((unsigned)time(NULL));//用时间初始化随机数种子库,unsigned无符号型
+	FILE* fp1,*fp2;
+	fopen_s(&fp1, "没有答案的题目.txt", "w");
+	fopen_s(&fp2, "有答案的题目.txt", "w");
 
-
-	for (j = 0; j < 100; j++)
+	for (j = 0; j < 100; j++)//生成随机数放进数组中
 	{
 		a[j] = rand() % 10;
 		b[j] = rand() % 10;
 		x[j] = rand() % 2;
 	}
-	for (j = 0; j < 100; j++)
+	for (j = 0; j < 100; j++)//没有答案的题目
 	{
+	
 		switch (x[j]) {
 		case(0):
 			if (a[j] < b[j])
-				printf("(%d) %d-%d=\n", j + 1, b[j], a[j]);
+				fprintf(fp1, "(%d) %d-%d=\n", j + 1, b[j], a[j]);
 			else
-				printf("(%d) %d-%d=\n", j + 1, a[j], b[j]);
+				fprintf(fp1, "(%d) %d-%d=\n", j + 1, a[j], b[j]);
 			break;
 		case(1):
-			printf("(%d) %d+%d=\n", j + 1, a[j], b[j]);
+			fprintf(fp1, "(%d) %d+%d=\n", j + 1, a[j], b[j]);
 			break;
+		
 		}
 	}
 	
-	for (j = 0; j < 100; j++)
+	for (j = 0; j < 100; j++)//有答案的题目
 	{
+		
 		switch (x[j]) {
 		case(0):
 			if (a[j] < b[j])
-				printf("(%d) %d-%d=%d\n", j + 1, b[j], a[j], b[j] - a[j]);
+				fprintf(fp2, "(%d) %d-%d=%d\n", j + 1, b[j], a[j], b[j] - a[j]);
 			else
-				printf("(%d) %d-%d=%d\n", j + 1, a[j], b[j], a[j] - b[j]);
+				fprintf(fp2, "(%d) %d-%d=%d\n", j + 1, a[j], b[j], a[j] - b[j]);
 			break;
 		case(1):
-			printf("(%d) %d+%d=%d\n", j + 1, a[j], b[j], a[j] + b[j]);
+			fprintf(fp2, "(%d) %d+%d=%d\n", j + 1, a[j], b[j], a[j] + b[j]);
 			break;
+		
 		}
 	}
+	fclose(fp1);
+	fclose(fp2);
 	return 0;
 }
