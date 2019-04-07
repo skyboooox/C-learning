fcf8b4b917b556893363d665e06b9d2d7ad21bb7
zcj924 <zcj924@gmail.com>
2019-04-08 05:28:29 +0800

    改进数组

--- C语言学习/10以内加减法(M2).c
+++ C语言学习/10以内加减法(M2).c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>//随机数
-#include <time.h>//可得到当前的时间（精确到秒）
+#include <time.h>//调用时间
 #include <math.h>//数学函数
 
 int main(int argc, char const* argv[])
 {
-	int a, b,x, i = 0;
+	int a[100], b[100],x[100], i = 0,j;
 	srand((unsigned)time(NULL));//用时间初始化随机数种子库,unsigned无符号型
 	
+
+	for (j=0;j<100;j++)
+	{
+		a[j] = rand() % 10;
+		b[j] = rand() % 10;
+		x[j] = rand() % 2;
+	}
+
+
 	while (i < 100) {
-		i++;
-		a = rand() % 10;
-		b = rand() % 10;
-		x = rand() % 2;
+		
+		
 	
 		switch (x) {
 		case(0):
 			if (a<b) 
-				printf("(%d) %d-%d=%d\n",i, b, a,b-a);
+				printf("(%d) %d-%d=%d\n",i+1, b, a,b-a);
 			else
-				printf("(%d)%d-%d=%d\n",i, a, b,a-b);
+				printf("(%d)%d-%d=%d\n",i+1, a, b,a-b);
 			break;
 		case(1):
-				printf("(%d)%d+%d=%d\n",i, a, b,a+b);
+				printf("(%d)%d+%d=%d\n",i+1, a, b,a+b);
 			break;
 				   }
+
+		i++;
 	}
 
 	return 0;
