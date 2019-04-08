5e5a377b7cc3eeccb732829f5d5b33ca4b5fa4aa
zcj924 <zcj924@gmail.com>
2019-04-08 15:01:52 +0800

    可以指定出题数目

--- C语言学习/10以内加减法(M2).c
+++ C语言学习/10以内加减法(M2).c
@@ -5,27 +5,22 @@
 
 int main(int argc, char const* argv[])
 {
-	int i;
-	printf("请输入你要出多少道题目,文件保存在程序目录下\n");
-	scanf_s("%d", &i);
-
-	
-	int a[10000], b[10000], x[10000], j;
+	int a[10000], b[10000], x[10000], j,i;
 	srand((unsigned)time(NULL));//用时间初始化随机数种子库,unsigned无符号型
 	FILE* fp1,*fp2;
 	//fp1 = fopen("1.txt", "w"); ????夹私货?fopen不让用????????
 	fopen_s(&fp1, "没有答案的题目.txt", "w");
 	fopen_s(&fp2, "有答案的题目.txt", "w");
 
-	for (j = 0; j < i; j++)//生成随机数放进数组中
-	{
+	printf("请输入你要出多少道题目(最多10000),文件保存在程序目录下\n");
+	scanf_s("%d", &i);
+
+	for (j = 0; j < i; j++) {//生成随机数放进数组中
 		a[j] = rand() % 10;
 		b[j] = rand() % 10;
 		x[j] = rand() % 2;
 	}
-	for (j = 0; j < i; j++)//没有答案的题目
-	{
-	
+	for (j = 0; j < i; j++) {//没有答案的题目
 		switch (x[j]) {
 		case(0):
 			if (a[j] < b[j])
@@ -36,13 +31,9 @@ int main(int argc, char const* argv[])
 		case(1):
 			fprintf(fp1, "(%d) %d+%d=\n", j + 1, a[j], b[j]);
 			break;
-		
 		}
 	}
-	
-	for (j = 0; j < i; j++)//有答案的题目
-	{
-		
+	for (j = 0; j < i; j++) {//有答案的题目
 		switch (x[j]) {
 		case(0):
 			if (a[j] < b[j])
@@ -53,7 +44,6 @@ int main(int argc, char const* argv[])
 		case(1):
 			fprintf(fp2, "(%d) %d+%d=%d\n", j + 1, a[j], b[j], a[j] + b[j]);
 			break;
-		
 		}
 	}
 	fclose(fp1);
