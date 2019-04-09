d56d6278baf05a2ff7854b3900d66456194695ff
zcj924 <zcj924@gmail.com>
2019-04-09 08:40:22 +0800

    增添注释

--- C语言学习/10以内加减法(M2).c
+++ C语言学习/10以内加减法(M2).c
@@ -12,50 +12,59 @@ j,循环次数
 */
 int main(int argc, char const* argv[])
 {
-	int a[10000], b[10000], x[10000], j,i;
+	int a[10000], b[10000], x[10000], j, i;
 	srand((unsigned)time(NULL));//用时间初始化随机数种子库,unsigned无符号型
-	FILE* fp1,*fp2;
+	FILE* fp1, * fp2;
 	//fp1 = fopen("1.txt", "w"); ????夹私货?fopen不让用????????
-		fopen_s(&fp1, "没有答案的题目.txt", "w");
-		fopen_s(&fp2, "有答案的题目.txt", "w");
+	fopen_s(&fp1, "没有答案的题目.txt", "w");
+	fopen_s(&fp2, "有答案的题目.txt", "w");
 
 	printf("请输入你要出多少道题目(最多10000),文件保存在程序目录下\n");
 	scanf_s("%d", &i);
-
-	for (j = 0; j < i; j++) {//生成随机数放进数组中
-		a[j] = rand() % 10;
-		b[j] = rand() % 10;
-		x[j] = rand() % 2;
+	if (i > 10000) {
+		printf("输入错误");
+		return 0;
 	}
-	for (j = 0; j < i; j++) {//没有答案的题目
-		switch (x[j]) {
-		case(0):
-			if (a[j] < b[j])
-				fprintf(fp1, "(%d) %d-%d=\n", j + 1, b[j], a[j]);
-			else
-				fprintf(fp1, "(%d) %d-%d=\n", j + 1, a[j], b[j]);
-			break;
-		case(1):
+	else {
+		for (j = 0; j < i; j++) {//生成随机数放进数组中
+			a[j] = rand() % 10;
+			b[j] = rand() % 10;
+			x[j] = rand() % 2;
+		}
+		for (j = 0; j < i; j++) {//没有答案的题目
+			switch (x[j]) {
+			case(0):
+				if (a[j] < b[j])
+					fprintf(fp1, "(%d) %d-%d=\n", j + 1, b[j], a[j]);
+				else
+					fprintf(fp1, "(%d) %d-%d=\n", j + 1, a[j], b[j]);
+				break;
+			case(1):
 				fprintf(fp1, "(%d) %d+%d=\n", j + 1, a[j], b[j]);
-			break;
+				break;
+			}
 		}
-	}
-	for (j = 0; j < i; j++) {//有答案的题目
-		switch (x[j]) {
-		case(0):
-			if (a[j] < b[j])
-				fprintf(fp2, "(%d) %d-%d=%d\n", j + 1, b[j], a[j], b[j] - a[j]);
-			else
-				fprintf(fp2, "(%d) %d-%d=%d\n", j + 1, a[j], b[j], a[j] - b[j]);
-			break;
-		case(1):
+		for (j = 0; j < i; j++) {//有答案的题目
+			switch (x[j]) {
+			case(0):
+				if (a[j] < b[j])
+					fprintf(fp2, "(%d) %d-%d=%d\n", j + 1, b[j], a[j], b[j] - a[j]);
+				else
+					fprintf(fp2, "(%d) %d-%d=%d\n", j + 1, a[j], b[j], a[j] - b[j]);
+				break;
+			case(1):
 				fprintf(fp2, "(%d) %d+%d=%d\n", j + 1, a[j], b[j], a[j] + b[j]);
-			break;
+				break;
+			}
 		}
+		fclose(fp1);
+		fclose(fp2);
+		if (i > 100)
+		{
+			printf("你怕是让小学生累死哦!!");
+		}
+		printf("生成成功,快去逮个小学生试试!!!!");
+		return 0;
+
 	}
-	fclose(fp1);
-	fclose(fp2);
-	if (i > 100)
-		printf("你怕是让小学生累死哦!!");
-	return 0;
 }
