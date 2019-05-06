8d2bb466c3bf8b6de6ec4abbd593923b7df1acd6
zcj924 <zcj924@gmail.com>
2019-05-06 10:11:16 +0800

    搜索一段字符串.从一个字符开始,到一个字符结束.输出中间一段.

--- C语言学习/查找字符串位置.c
+++ C语言学习/查找字符串位置.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>//随机数
-#include <time.h>//调用时间
-#include <math.h>//数学函数
-#include <string.h>
-#define MAXSIZE 84997
+//祖传头文件
+#include <assert.h>//防御式编程
+#include <ctype.h>//检测和转换单个字
+#include <errno.h>//错误代码有关
+#include <float.h>//浮点值相关的依赖于平台的常量
+#include <limits.h>//各种变量类型的各种属性
+#include <locale.h>//特定地域的设置，比如日期格式和货币符号
+#include <math.h>//数学函数,所有 double 类型的参数
+#include <setjmp.h>//存储宏 setjmp() 和函数 longjmp() 
+#include <signal.h>//变量类型 sig_atomic_t、两个函数调用和一些宏来处理程序执行期间报告的不同信号。
+#include <stdarg.h>//一个变量类型 va_list 和三个宏，这三个宏可用于在参数个数未知（即参数个数可变）时获取函数中的参数。
+#include <stddef.h>//
+#include <stdio.h>//各种函数来执行输入和输出
+#include <stdlib.h>//四个变量类型、一些宏和各种通用工具函数
+#include <string.h>//字符串
+#include <time.h>//操作日期和时间
+#define MAXSIZE 84998//定义最大个数(VS2019通过测试) 
 
 /*
 搜索一段字符串
 从一个字符开始,到一个字符结束.输出中间一段.
 */
-int main()
-{
+int main(int argc, char const* argv[]){
 	char a[] = "aciuahkjhiuzxiuhaciuhkntrzx";//准备 要搜索的 数组
 	char* sf = strstr(a, "ac");
 	char* sl = strstr(a, "zx");
@@ -19,7 +29,7 @@ int main()
 	int i = 0;//计数器
 	while (sf != NULL) {
 		xf[i] = sf - a;
-		sf = strstr(sf + 1, "ac");
+		sf = strstr(sf + 1, "ax");
 		i++;
 	}
 	i = 0;//初始化计数器
@@ -35,8 +45,7 @@ print:
 	if (f < 0)//vs2019没有用到的数组元素是-858993460
 		return 0;
 	l = xl[i];
-	while (l >= f)
-	{
+	while (l >= f){
 		printf("%c", a[f]);
 		f++;
 	}
